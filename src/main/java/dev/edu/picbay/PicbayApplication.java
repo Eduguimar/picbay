bdd410f14819fc87c1fe7c29e8a1b57e891515f2
Eduardo <eduguimara@gmail.com>
2025-01-28 20:54:01 -0300

    Initial commit

---
+++ src/main/java/dev/edu/picbay/PicbayApplication.java
@@ -0,0 +1,13 @@
+package dev.edu.picbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicbayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicbayApplication.class, args);
+	}
+
+}
